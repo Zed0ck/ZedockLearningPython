a7da9cdaf0ef6320c5875d3b296eb3d413e15086
Mika Vallin <mika.vallin@gmail.com>
2021-10-01 18:25:56 +0300

    Create lessonOne.py

---
+++ lessonOne.py
@@ -0,0 +1,10 @@
+# This program says hello and asks my name
+
+print('Hello World!')
+print('What is your name?')
+myName = input()
+print('Nice to meet you ' + myName)
+
+# In this lesson we learn how to print strings. 
+# we also learn how to use input to get users name and store it to variable. 
+# Then we use that variable to print users name with a string.
